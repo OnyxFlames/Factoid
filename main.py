b7144447e1fc28e2d6be4d92533404359615a0c1
OnyxFlames <jacobhicks1996@hotmail.com>
2017-03-06 16:34:17 -0500

    Started work for the database. Added test-signup.html to work on safe ways to create accounts. Tweaked temp user.html file.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 
+import sqlite3
+from flask import g
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,12 +25,45 @@ def signup():
 def play():
     return render_template("play.html")
 
-@app.route('/user/<name>')
-def user(name):
+@app.route('/users/')
+@app.route('/users/<name>')
+def user(name=None):
     return render_template("user.html", name=name)
+
+@app.route('/test/signup/')
+@app.route('/test/signup.html')
+def adduser():
+    return render_template("test-signup.html")
 
 @app.errorhandler(404)
 def pagenotfound(err):
     return render_template("not_found.html"), 404
 
+# Database stuff
+
+@app.route('/admin/createdb/')
+def createdb():
+    with app.app_context():
+        get_db().execute("create table if not exists USER (id INT IDENTITY, firstname varchar(25),"
+                         " lastname varchar(30), constraint pk_id_constraint PRIMARY KEY(id));")
+    return "<h1>Admin Control Panel</h1>"
+
+DATABASE = 'database/datebase.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 app.run()
+
+with app.app_context():
+    get_db().execute("create table USER (id INT IDENTITY, firstname varchar(25), lastname varchar(30),"
+    "constraint pk_id_constraint PRIMARY KEY(id));")
