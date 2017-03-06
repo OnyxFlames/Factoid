5952f7eec225d36675e937fc87d55e001b4bd8cf
OnyxFlames <jacobhicks1996@hotmail.com>
2017-03-06 15:19:04 -0500

    Added CONTRIBUTING.md Updated README.md Added routes and templates for admin, signup, play, user, and 404 error handler.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Factoid
+A small flash-card based web application built from the [Spaced Repetition](https://en.wikipedia.org/wiki/Spaced_repetition) learning technique.
+Built on with Python 3.6.0 + Flask for the webapp, and using vanilla HTML/JS/CSS for the web stack.
--- main.py
+++ main.py
@@ -7,9 +7,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/admin/')
 @app.route('/admin.html')
 def admin():
     return render_template('admin.html')
 
+@app.route('/signup/')
+@app.route('/signup.html')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/play')
+@app.route('/play.html')
+def play():
+    return render_template("play.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+@app.errorhandler(404)
+def pagenotfound(err):
+    return render_template("not_found.html"), 404
 
 app.run()
